6810942a3b84ead4ee2554e783b25ef595dada1a
Gabe Ragland <gabe.ragland@gmail.com>
2018-05-22 11:06:47 -0700

    Simplify default theme

--- themes/default.js
+++ themes/default.js
@@ -3,14 +3,46 @@ export default {
   breakpoints: ['576px', '768px', '992px', '1200px'],
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512 ],
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+  // Custom style props go in the props object
   props: {
-    // Use custom props if you want:
-    // - Shorthand prop names
-    // - To hook into theme values
-    // - Advanced logic (see width prop below)
-
-    d: 'display',
-
+    f: {
+      // Where to find values in theme
+      themeKey: 'fontSizes',
+      // Default unit if none specified
+      defaultUnit: 'px',
+      // Resulting css property
+      style: 'fontSize'
+    },
+    color: {
+      // Extend theme and add colors object
+      themeKey: 'colors',
+      style: 'color'
+    },
+    bg: {
+      themeKey: 'colors',
+      style: 'backgroundColor'
+    },
+    borderColor: {
+      themeKey: 'colors',
+      style: 'borderColor'
+    },
+    d: {
+      style: 'display'
+    },
+    p: {
+      themeKey: 'space',
+      defaultUnit: 'px',
+      style: 'padding',
+      // Directional variations
+      variations: {
+        pt: 'paddingTop',
+        pr: 'paddingRight',
+        pb: 'paddingBottom',
+        pl: 'paddingLeft',
+        px: ['paddingLeft', 'paddingRight'],
+        py: ['paddingTop', 'paddingBottom']
+      }
+    },
     m: {
       themeKey: 'space',
       defaultUnit: 'px',
@@ -24,26 +56,16 @@ export default {
         my: ['marginTop', 'marginBottom']
       }
     },
-
-    p: {
-      themeKey: 'space',
-      defaultUnit: 'px',
-      style: 'padding',
-      variations: {
-        pt: 'paddingTop',
-        pr: 'paddingRight',
-        pb: 'paddingBottom',
-        pl: 'paddingLeft',
-        px: ['paddingLeft', 'paddingRight'],
-        py: ['paddingTop', 'paddingBottom']
-      }
+    h: {
+      style: 'height'
     },
-
+    // Advanced width prop
     w: {
+      // Style is a function instead of a string
       style: value => {
         let width = value;
-        // Make 1 = 100%, 1/2 = 50%, 1/3 = 33.33...%
-        // Helpful for setting column widths
+        // If less than 1 make it a fraction of 100% (1/3 = 33.33...%, etc)
+        // Nice for column widths (<Row><Col w={1/3}><Col w={2/3}></Row>)
         if (isNumber(width) && width <= 1) {
           width = `${width * 100}%`;
         }
@@ -51,44 +73,8 @@ export default {
           width: width
         };
       }
-    },
-
-    h: 'height',
-
-    color: {
-      // To use add a colors object to your theme
-      themeKey: 'colors',
-      style: 'color'
-    },
-
-    bg: {
-      themeKey: 'colors',
-      style: 'backgroundColor'
-    },
-
-    borderColor: {
-      themeKey: 'colors',
-      style: 'borderColor'
-    },
-
-    f: {
-      themeKey: 'fontSizes',
-      defaultUnit: 'px',
-      style: 'fontSize'
-    },
-
-    fontFamily: {
-      // To use add a fonts object to your theme
-      themeKey: 'fonts',
-      style: 'fontFamily'
-    },
-
-    fontWeight: {
-      // To use add a fontWeights object to your theme
-      themeKey: 'fontWeights',
-      style: 'fontWeight'
     }
   }
-};
+}
 
 const isNumber = n => typeof n === "number" && !isNaN(n);
